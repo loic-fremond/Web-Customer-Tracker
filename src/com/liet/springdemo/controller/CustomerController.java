421d50fa4bc6fc73ffab929c5b127aac44ffec1d
loic-fremond <loicfremond.dev@gmail.com>
2022-07-17 16:46:33 +0200

    updated controller to read sort field

--- src/com/liet/springdemo/controller/CustomerController.java
+++ src/com/liet/springdemo/controller/CustomerController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.liet.springdemo.entity.Customer;
 import com.liet.springdemo.service.CustomerService;
+import com.liet.springdemo.util.SortUtils;
 
 @Controller
 @RequestMapping("/customer")
@@ -23,10 +24,19 @@ public class CustomerController {
 	private CustomerService customerService;
 	
 	@GetMapping("/list")
-	public String listCustomers(Model theModel) {
+	public String listCustomers(Model theModel, @RequestParam(required=false) String sort) {
 		
 		// get customers from the service
-		List<Customer> theCustomers = customerService.getCustomers();
+		List<Customer> theCustomers = null;
+		
+		// check for sort field
+		if (sort != null) {
+			int theSortField = Integer.parseInt(sort);
+			theCustomers = customerService.getCustomers(theSortField);
+		} else {
+			// no sort field provided -> default to sorting by last name
+			theCustomers=customerService.getCustomers(SortUtils.LAST_NAME);
+		}
 		
 		// add the customers to the model
 		theModel.addAttribute("customers", theCustomers);
@@ -70,7 +80,7 @@ public class CustomerController {
 		return "redirect:/customer/list";
 	}
 	
-	@GetMapping("search")
+	@GetMapping("/search")
 	public String searchCustomers(@RequestParam("theSearchName") String theSearchName, Model theModel) {
 		// search customers from the service
 		List<Customer> theCustomers = customerService.searchCustomers(theSearchName);
